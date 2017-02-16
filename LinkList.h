335a9da8c129bd0111078478cecb780f576445b9
HorizonVim <HorizonVim>
2017-02-16 10:10:49 +0800

    add LinkList.h

---
+++ LinkList.h
@@ -0,0 +1,17 @@
+#ifndef	_LinkList_h_
+#define	_LinkList_h_
+
+template<typename T>
+class LinkList{
+public:
+	typedef	Index unsigned int;
+
+	LinkList()=default;
+	virtual ~LinkList()=default;
+
+	//virtual LinkList* insert(Index id,T& data);
+	//virtual
+
+};
+
+#endif
